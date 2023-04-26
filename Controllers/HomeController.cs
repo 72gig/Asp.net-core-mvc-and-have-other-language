0af5d4ee647993046710d1193ec4e76fc8979fe9
jaitslin <jaitslin@heavyhitter.com.tw>
2023-04-26 11:04:51 +0800

    調整完成

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using IActionResultExample.Models;
 
 namespace IActionResultExample.Controllers
 {
@@ -18,6 +19,21 @@ namespace IActionResultExample.Controllers
             //return RedirectPermanent($"ReportInput/{type}");
         }
 
+        [Route("ReportInput/{type?}/{startdate?}/{enddate?}")]
+        //Url: http://localhost:5267/ReportInput?type=purchase&startdate=2023-04-11&enddate=2023-04-27
+        public IActionResult ReportData([FromQuery]string? type,
+                                        [FromQuery]string? startdate,
+                                        [FromQuery]string? enddate,
+                                        Readsql setting)
+        {
+            var qrerydata = Request.QueryString;
+
+            // 傳到Model 分析
+            sqlUser data = new sqlUser();
+            var result =  data.selectSql(setting);
+
+            return Content($"<h1>test {result}<h1>", "text/html");
+        }
     }
 
 }
